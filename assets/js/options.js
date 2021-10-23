89c2d6bcb32a6e8abc154451b87161591df7202c
Danny Winning <dan@dannywinning.co.uk>
2021-10-23 18:08:09 +0100

    small restructure, using scss now

---
+++ assets/js/options.js
@@ -0,0 +1,11 @@
+function saveOptions()
+{
+    let showClock = document.querySelector("#display-clock-checkbox").checked;
+}
+
+function restoreOptions()
+{
+
+}
+
+document.querySelector("#save-options").addEventListener("click", saveOptions);
