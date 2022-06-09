8500501c52ea21ff954fad38f5300f36079e76d2
Daniel Winning <daniel@winningwebdesign.co.uk>
2022-06-09 20:06:29 +0100

    minor css tweaks, displaying version in popup

--- assets/js/focus.js
+++ assets/js/focus.js
@@ -1,5 +1,22 @@
 "use strict";
 
-document.querySelector("#settings-page").addEventListener("click", function () {
-  chrome.runtime.openOptionsPage();
+window.addEventListener("DOMContentLoaded", function () {
+  document.querySelector("#settings-page").addEventListener("click", function () {
+    chrome.runtime.openOptionsPage();
+  });
+  showVersion();
 });
+
+function showVersion() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "/manifest.json");
+
+  xhr.onreadystatechange = function () {
+    if (xhr.status === 200 && xhr.readyState === 4) {
+      var response = xhr.responseText;
+      document.querySelector("#version-ref").innerHTML += " " + JSON.parse(response).version;
+    }
+  };
+
+  xhr.send();
+}
--- src/js/focus.js
+++ src/js/focus.js
@@ -1,3 +1,23 @@
-document.querySelector("#settings-page").addEventListener("click", () => {
-    chrome.runtime.openOptionsPage();
+window.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("#settings-page").addEventListener("click", () => {
+        chrome.runtime.openOptionsPage();
+    });
+
+    showVersion();
 });
+
+function showVersion()
+{
+    let xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "/manifest.json");
+
+    xhr.onreadystatechange = () => {
+        if (xhr.status === 200 && xhr.readyState === 4) {
+            let response = xhr.responseText;
+
+            document.querySelector("#version-ref").innerHTML +=  " " + JSON.parse(response).version;
+        }
+    }
+    xhr.send();
+}
