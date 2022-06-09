89cf2ad3f8ea733399776875b45283f9eea98630
Daniel Winning <daniel@winningwebdesign.co.uk>
2022-06-09 15:04:40 +0100

    added build process

--- assets/js/options.js
+++ assets/js/options.js
@@ -1,68 +1,63 @@
-function saveOptions()
-{
-    let saveButton = document.querySelector("#save-options"),
-        spinner = document.querySelector("#spinner"),
-        showClock = document.querySelector("#clock-switch").querySelector("input[type='checkbox']").checked,
-        greetByName = document.querySelector("#name-greeting-switch").querySelector("input[type='checkbox']").checked,
-        username = document.querySelector("#username-input").value,
-        feedbackElement = document.querySelector("#feedback"),
-        currentTheme = document.querySelector(".current").dataset.name;
-
-    saveButton.classList.add("hidden");
-    spinner.classList.remove("hidden");
-
-    chrome.storage.sync.set({
-        showClock: showClock,
-        greetByName: greetByName,
-        username: username,
-        theme: currentTheme
-    }).then(response => {
-        let feedbackElement = document.querySelector("#feedback");
-
-        spinner.classList.add("hidden");
-        saveButton.classList.remove("hidden");
-        feedbackElement.classList.remove("hidden");
-        feedbackElement.classList.add("success");
-        feedbackElement.innerHTML = "Settings saved!";
-
-        window.setTimeout(() => {
-            feedbackElement.classList.add("hidden");
-        }, 3000);
-    });
+function saveOptions() {
+  let saveButton = document.querySelector("#save-options"),
+      spinner = document.querySelector("#spinner"),
+      showClock = document.querySelector("#clock-switch").querySelector("input[type='checkbox']").checked,
+      greetByName = document.querySelector("#name-greeting-switch").querySelector("input[type='checkbox']").checked,
+      username = document.querySelector("#username-input").value,
+      feedbackElement = document.querySelector("#feedback"),
+      currentTheme = document.querySelector(".current").dataset.name;
+  saveButton.classList.add("hidden");
+  spinner.classList.remove("hidden");
+  chrome.storage.sync.set({
+    showClock: showClock,
+    greetByName: greetByName,
+    username: username,
+    theme: currentTheme
+  }).then(response => {
+    let feedbackElement = document.querySelector("#feedback");
+    spinner.classList.add("hidden");
+    saveButton.classList.remove("hidden");
+    feedbackElement.classList.remove("hidden");
+    feedbackElement.classList.add("success");
+    feedbackElement.innerHTML = "Settings saved!";
+    window.setTimeout(() => {
+      feedbackElement.classList.add("hidden");
+    }, 3000);
+  });
 }
 
-function retrieveOptions()
-{
-    chrome.storage.sync.get(["showClock", "greetByName", "username", "theme"]).then(response => {
-        if (response.showClock !== undefined && response.showClock) {
-            document.querySelector("#clock-switch").querySelector("input[type='checkbox']").checked = true;
-        }
-        if (response.greetByName !== undefined && response.greetByName) {
-            document.querySelector("#name-greeting-switch").querySelector("input[type='checkbox']").checked = true;
-        }
-        if (response.username !== undefined) {
-            document.querySelector("#username-input").value = response.username;
-        }
-        if (response.theme === undefined) {
-            document.querySelector("[data-name='default']").classList.add("current");
-        } else {
-            document.querySelector(`[data-name='${response.theme}']`).classList.add("current");
-        }
-    });
+function retrieveOptions() {
+  chrome.storage.sync.get(["showClock", "greetByName", "username", "theme"]).then(response => {
+    if (response.showClock !== undefined && response.showClock) {
+      document.querySelector("#clock-switch").querySelector("input[type='checkbox']").checked = true;
+    }
+
+    if (response.greetByName !== undefined && response.greetByName) {
+      document.querySelector("#name-greeting-switch").querySelector("input[type='checkbox']").checked = true;
+    }
+
+    if (response.username !== undefined) {
+      document.querySelector("#username-input").value = response.username;
+    }
+
+    if (response.theme === undefined) {
+      document.querySelector("[data-name='default']").classList.add("current");
+    } else {
+      document.querySelector(`[data-name='${response.theme}']`).classList.add("current");
+    }
+  });
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-    retrieveOptions();
-    document.querySelector("#save-options").addEventListener("click", saveOptions);
-
-    let themeOptions = document.querySelectorAll(".theme");
-
-    themeOptions.forEach(theme => {
-       theme.addEventListener("click", () => {
-          themeOptions.forEach(theme => {
-             theme.classList.remove("current");
-          });
-          theme.classList.add("current");
-       });
+  retrieveOptions();
+  document.querySelector("#save-options").addEventListener("click", saveOptions);
+  let themeOptions = document.querySelectorAll(".theme");
+  themeOptions.forEach(theme => {
+    theme.addEventListener("click", () => {
+      themeOptions.forEach(theme => {
+        theme.classList.remove("current");
+      });
+      theme.classList.add("current");
     });
+  });
 });
--- assets/js/tab/main.js
+++ assets/js/tab/main.js
@@ -4,73 +4,67 @@
  * @param domElement
  * @return void
  */
-function getGreeting(domElement)
-{
-    let currentDate = new Date,
-        currentHour = currentDate.getHours(),
-        greeting = "Good ";
+function getGreeting(domElement) {
+  let currentDate = new Date(),
+      currentHour = currentDate.getHours(),
+      greeting = "Good ";
 
-    if (currentHour < 12) {
-        greeting += "morning";
-    } else if (currentHour >= 12 && currentHour < 17) {
-        greeting += "afternoon";
-    } else {
-        greeting += "evening";
+  if (currentHour < 12) {
+    greeting += "morning";
+  } else if (currentHour >= 12 && currentHour < 17) {
+    greeting += "afternoon";
+  } else {
+    greeting += "evening";
+  }
+
+  document.querySelector(domElement).innerHTML = `${greeting}!`;
+  displayClock(domElement);
+}
+
+function displayClock(greetingElement) {
+  chrome.storage.sync.get(["showClock", "username", "greetByName"]).then(response => {
+    let showClock = response.showClock,
+        username = response.username,
+        greetByName = response.greetByName;
+
+    if (showClock !== undefined && showClock) {
+      let clockElement = document.querySelector("#clock");
+      clockElement.innerHTML = getTimeString();
+      clockElement.classList.remove("hidden");
     }
 
-    document.querySelector(domElement).innerHTML = `${greeting}!`;
-    displayClock(domElement);
+    if (username !== undefined && username !== "" && greetByName !== undefined && greetByName) {
+      let greetingHeading = document.querySelector(greetingElement);
+      greetingHeading.innerHTML = greetingHeading.innerHTML.replace("!", "") + `, ${username}!`;
+    }
+  });
 }
 
-function displayClock(greetingElement)
-{
-    chrome.storage.sync.get(["showClock", "username", "greetByName"]).then(response => {
-        let showClock = response.showClock,
-            username = response.username,
-            greetByName = response.greetByName;
-
-        if (showClock !== undefined && showClock) {
-            let clockElement = document.querySelector("#clock");
-
-            clockElement.innerHTML = getTimeString();
-            clockElement.classList.remove("hidden");
-        }
-        if (username !== undefined && username !== "" && greetByName !== undefined && greetByName) {
-            let greetingHeading = document.querySelector(greetingElement);
-            greetingHeading.innerHTML = greetingHeading.innerHTML.replace("!", "") + `, ${username}!`;
-        }
-    });
-}
-
-function getTimeString()
-{
-    let date = new Date,
-        hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours(),
-        minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
-
-    return hours + ":" + minutes;
+function getTimeString() {
+  let date = new Date(),
+      hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours(),
+      minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+  return hours + ":" + minutes;
 }
 
 function show(domElement) {
-    document.querySelector(domElement).classList.remove("hidden");
+  document.querySelector(domElement).classList.remove("hidden");
 }
 
-function getStylesheet()
-{
-    chrome.storage.sync.get("theme").then(response => {
-        let theme = response.theme === undefined ? "default" : response.theme;
-        document.querySelector("head").insertAdjacentHTML("beforeend", `<link rel="stylesheet" type="text/css" href="../assets/css/themes/${theme}.css">`);
-        window.setTimeout(() => {
-            document.querySelector(".overlay").classList.add("hidden");
-        }, 250); // Artificial delay because it's slightly less annoying than the flash
-    });
+function getStylesheet() {
+  chrome.storage.sync.get("theme").then(response => {
+    let theme = response.theme === undefined ? "default" : response.theme;
+    document.querySelector("head").insertAdjacentHTML("beforeend", `<link rel="stylesheet" type="text/css" href="../assets/css/themes/${theme}.css">`);
+    window.setTimeout(() => {
+      document.querySelector(".overlay").classList.add("hidden");
+    }, 250); // Artificial delay because it's slightly less annoying than the flash
+  });
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-    getStylesheet();
-    getGreeting("#greeting");
-
-    window.setTimeout(() => {
-        show(".content");
-    }, 200);
+  getStylesheet();
+  getGreeting("#greeting");
+  window.setTimeout(() => {
+    show(".content");
+  }, 200);
 });
